5672ec1e3896a524c59d128a2f028f189b5bbefd
cesarsiguencia <cesar.siguencia@gmail.com>
2022-02-16 21:37:43 -0500

    html generated

--- app.js
+++ app.js
@@ -8,25 +8,45 @@
 // // slice is for anything past the 2 arrays of info, which would be the node info
 // console.log(profileDataArgs);
 
+//LESSON 1
+
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
+// //This
+
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i < profileDataArr.length; i++){
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('==============')
+//     //is the same as this
+//     profileDataArr.forEach((profileItem) =>
+//         console.log(profileItem)
+//     )
+// }
+
+// printProfileData(profileDataArgs)
+
+//LESSON 2
+
+const [name, github] = profileDataArgs;
 
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-//This
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('==============')
-    //is the same as this
-    profileDataArr.forEach((profileItem) =>
-        console.log(profileItem)
-    )
-}
+// console.log(name,github)
+// console.log(generatePage(name, github));
 
 
 
-printProfileData(profileDataArgs)
+//3 arguements to generate file. 1- write file name, 2 - data that will write in file. 3- err arguement if failed or console if success
+fs.writeFile('./index.html', generatePage(name,github), err =>  {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+})
 
