0ecd4295f28f0ba18581fa6d1ca951b83de86802
cesarsiguencia <cesar.siguencia@gmail.com>
2023-06-02 08:55:31 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# portfolio-generator
+# Portfolio-Generator-With-Node
+
+This application is my first full Node.js project that generates a quick personal portfolio html page using the Inquirer npm package and answering personal questions about yourself and a few of your projects through the command line. 
+
+After answering all the questions, your portfolio will be generated under dist/.
+
+Inquirer Questions:
+1: What is your name?
+2: Github username
+3: About section
+4: Project name
+5: Project Description
+6: Project technologies
+7: Project Github link
+8: Feature or do not feature project on portfolio?
+9: Add another project confirm?
+
+
+### Installation
+
+Download the application to your local drive, open the command line with the application or the root and run 'Node app.js'
+
+After you have completed answering all your questions, the generated index.html will appear under dist/
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+// const fs = require('fs'); -NO LONGER NEEDED SINCE IT WAS MOVED TO GENERATE-SITE.JS
+const { writeFile, copyFile } = require('./utils/generate-site.js')
 const generatePage = require('./src/page-template.js')
 
 const inquirer = require('inquirer');  // the package that allows us to use questionaire in the terminal
@@ -48,7 +49,7 @@ const promptUser = () => {
 }
 
 
-const promptProject = portfolioData => {
+const promptProject = portfolioData => { //function accepting info from promptUser as a parameter, also adding project info below when returned below
 
     // If there's no 'projects' array property, create one
      if (!portfolioData.projects) {
@@ -134,30 +135,73 @@ const promptProject = portfolioData => {
 
 
 // in this order because we want promptUser first then promptProject question once the first have been answered, in sequence order
-promptUser()
-    // .then(answers => console.log(answers)) -- THIS IS BAD FOR MY CODE
+// promptUser()
+//     // .then(answers => console.log(answers)) -- THIS IS BAD FOR MY CODE
+//     .then(promptProject)
+
+//     .then(portfolioProjectData => {
+//         console.log(portfolioProjectData);
+
+//         const pageHTML = generatePage(portfolioProjectData);
+
+//         fs.writeFile('./dist/index.html', pageHTML, err => {
+//             if (err) throw new Error(err);
+
+//         console.log('Page created! Check out index.html in this directory to see it!');
+//         });
+
+//         fs.copyFile('./src/style.css', './dist/style.css', err => {
+//           if (err) {
+//             console.log(err);
+//             return;
+//           }
+//           console.log('Style sheet copied successfully!');
+//         });
+//     }  
+//     );
+
+// =======================
+// using PROMISES instead of call back functions above
+
+    promptUser() // after promptUser is complete, go to promptProject below
     .then(promptProject)
 
-    .then(portfolioProjectData => {
-        console.log(portfolioProjectData);
+    .then(portfolioProjectData => { //data is now in parameter from promptProject, because of then
+      return generatePage(portfolioProjectData) //requiring the HTML content from page-template with our data, which will return the finished HTML template code into PAGEHTML
 
-        const pageHTML = generatePage(portfolioProjectData);
+    })
+      .then(pageHTML => { //passing PAGEHTML into newly created writeFile(), which returns a promise from GENERATE-SITE.js
+        return writeFile(pageHTML)
+      })  
 
-        fs.writeFile('./dist/index.html', pageHTML, err => {
-            if (err) throw new Error(err);
+      .then(writeFileResponse => { // Upon a successful file creation, we take the writeFileResponse object provided by the writeFile() function's resolve() execution to log it, and then we return copyFile(), to provide the css file. 
+        console.log(writeFileResponse);
+        return copyFile();
+      })
+      .then(copyFileResponse => {
+        console.log(copyFileResponse)
+      })
+      .catch(err =>{ //catch our errors
+        console.log(err)
+      })
 
-        console.log('Page created! Check out index.html in this directory to see it!');
-        });
+      
 
-        fs.copyFile('./src/style.css', './dist/style.css', err => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-          console.log('Style sheet copied successfully!');
-        });
-    }  
-    );
+        // fs.writeFile('./dist/index.html', pageHTML, err => {
+        //     if (err) throw new Error(err);
+
+        // console.log('Page created! Check out index.html in this directory to see it!');
+        // });
+
+        // fs.copyFile('./src/style.css', './dist/style.css', err => {
+        //   if (err) {
+        //     console.log(err);
+        //     return;
+        //   }
+        //   console.log('Style sheet copied successfully!');
+        // });
+    
+    // );
 
 
     
